1c84561700b842e7a65d63aa8f3a63f4b43c8cba
r0227n <developryo@gmail.com>
2025-04-05 12:06:22 +0900

    fix: typeにdefaultを設定

--- cli/gen_article/bin/gen_article.dart
+++ cli/gen_article/bin/gen_article.dart
@@ -12,12 +12,14 @@ void main(List<String> arguments) async {
     return emojis[random].emoji;
   }
 
+  const defaultType = 'weekly';
+
   final parser =
       ArgParser()
         ..addOption('title', abbr: 't', help: '記事のタイトル')
         ..addOption('date', abbr: 'd', help: '記事の日付 (YYYY-MM-DD)')
         ..addOption('emoji', abbr: 'e', help: '記事の絵文字（未指定時はランダム）')
-        ..addOption('type', help: '記事のタイプ (monthly/weekly/daily)')
+        ..addOption('type', help: '記事のタイプ (monthly/weekly/daily)', defaultsTo: defaultType)
         ..addOption('topics', abbr: 'p', help: '記事のトピック（カンマ区切りで複数指定可: Flutter,Dart）')
         ..addFlag('published', help: '公開状態（true/false）', defaultsTo: false)
         ..addFlag('help', abbr: 'h', help: 'ヘルプを表示', negatable: false);
@@ -35,7 +37,7 @@ void main(List<String> arguments) async {
       results['emoji'] != null && (results['emoji'] as String).isNotEmpty
           ? results['emoji']
           : getRandomEmoji();
-  final type = results['type'];
+  final type = results['type'] ?? defaultType;
   final topics =
       results['topics'] != null && (results['topics'] as String).isNotEmpty
           ? (results['topics'] as String).split(',').map((e) => e.trim()).toList()
