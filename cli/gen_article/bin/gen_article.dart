d1336b174e0356e2038f806732ec5951faa4c2d4
r0227n <developryo@gmail.com>
2025-04-05 11:07:35 +0900

    add: args package

--- cli/gen_article/bin/gen_article.dart
+++ cli/gen_article/bin/gen_article.dart
@@ -1,17 +1,24 @@
+import 'dart:io';
+
 import 'package:gen_article/gen_article.dart' as gen_article;
+import 'package:args/args.dart';
 
 void main(List<String> arguments) async {
-  String? title;
-  String? date;
+  final parser =
+      ArgParser()
+        ..addOption('title', abbr: 't', help: '記事のタイトル')
+        ..addOption('date', abbr: 'd', help: '記事の日付 (YYYY-MM-DD)')
+        ..addFlag('help', abbr: 'h', help: 'ヘルプを表示', negatable: false);
 
-  // 引数の処理
-  if (arguments.isNotEmpty) {
-    title = arguments[0];
+  final results = parser.parse(arguments);
+
+  if (results['help']) {
+    print(parser.usage);
+    exit(0);
   }
 
-  if (arguments.length > 1) {
-    date = arguments[1];
-  }
+  final title = results['title'];
+  final date = results['date'];
 
   await gen_article.generateArticle(title: title, date: date);
 }
