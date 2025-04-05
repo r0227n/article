1bed4a23b195f646bd884b6326669c16d8608267
r0227n <developryo@gmail.com>
2025-04-05 10:40:15 +0900

    add: 記事作成CLI叩き台作成

---
+++ cli/gen_article/test/gen_article_test.dart
@@ -0,0 +1,8 @@
+import 'package:gen_article/gen_article.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ cli/templates/article_template.md
@@ -0,0 +1,7 @@
+---
+title: ""
+emoji: "📓"
+type: "weekly" # monthly: 月次記事 / weekly: 週次記事 / daily: 日次記事
+topics: []
+published: false
+---
