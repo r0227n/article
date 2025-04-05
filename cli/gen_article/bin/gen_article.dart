718dd6be585569abd2bd5263d541fe0e855f82e8
r0227n <developryo@gmail.com>
2025-04-05 11:53:32 +0900

    add: randomでemojiを代入可能にした

--- cli/gen_article/bin/gen_article.dart
+++ cli/gen_article/bin/gen_article.dart
@@ -1,14 +1,22 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:gen_article/gen_article.dart' as gen_article;
 import 'package:args/args.dart';
+import 'package:unicode_emojis/unicode_emojis.dart';
 
 void main(List<String> arguments) async {
+  String getRandomEmoji() {
+    final emojis = UnicodeEmojis.allEmojis;
+    final random = Random().nextInt(emojis.length);
+    return emojis[random].emoji;
+  }
+
   final parser =
       ArgParser()
         ..addOption('title', abbr: 't', help: '記事のタイトル')
         ..addOption('date', abbr: 'd', help: '記事の日付 (YYYY-MM-DD)')
-        ..addOption('emoji', abbr: 'e', help: '記事の絵文字')
+        ..addOption('emoji', abbr: 'e', help: '記事の絵文字（未指定時はランダム）')
         ..addOption('type', help: '記事のタイプ (monthly/weekly/daily)')
         ..addOption('topics', abbr: 'p', help: '記事のトピック（カンマ区切りで複数指定可: Flutter,Dart）')
         ..addFlag('published', help: '公開状態（true/false）', defaultsTo: false)
@@ -23,7 +31,10 @@ void main(List<String> arguments) async {
 
   final title = results['title'];
   final date = results['date'];
-  final emoji = results['emoji'];
+  final emoji =
+      results['emoji'] != null && (results['emoji'] as String).isNotEmpty
+          ? results['emoji']
+          : getRandomEmoji();
   final type = results['type'];
   final topics =
       results['topics'] != null && (results['topics'] as String).isNotEmpty
