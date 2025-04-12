f969a21eab127429efd4b368bbb2594d91bd31c7
r0227n <developryo@gmail.com>
2025-04-13 03:59:45 +0900

    feat: add share functionality to ArticleContentScreen
    * add: ShareActionMenu widget for sharing article URL
    * edit: include share button in AppBar with URL copy option
    * update: display snackbar confirmation for URL copy action

--- app/article_viewer/lib/ui/article/widgets/article_content_screen.dart
+++ app/article_viewer/lib/ui/article/widgets/article_content_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:article_viewer/data/services/article_service.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../domain/models/markdown_cotent.dart';
 
@@ -35,7 +37,15 @@ class _ArticleContentScreenState extends ConsumerState<ArticleContentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(_articleTitle ?? '記事')),
+      appBar: AppBar(
+        title: Text(_articleTitle ?? '記事'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 12.0),
+            child: ShareActionMenu(),
+          ),
+        ],
+      ),
       body: FutureBuilder<MarkdownCotent>(
         future: _contentFuture,
         builder: (context, snapshot) {
@@ -50,6 +60,69 @@ class _ArticleContentScreenState extends ConsumerState<ArticleContentScreen> {
           return const Center(child: CircularProgressIndicator());
         },
       ),
+    );
+  }
+}
+
+class ShareActionMenu extends StatefulWidget {
+  const ShareActionMenu({super.key});
+
+  @override
+  State<ShareActionMenu> createState() => _ShareActionMenuState();
+}
+
+class _ShareActionMenuState extends State<ShareActionMenu> {
+  final FocusNode _buttonFocusNode = FocusNode(debugLabel: 'Menu Button');
+
+  @override
+  void dispose() {
+    _buttonFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuAnchor(
+      childFocusNode: _buttonFocusNode,
+      menuChildren: <Widget>[
+        MenuItemButton(
+          onPressed: () async {
+            final url = Uri.decodeComponent(
+              GoRouter.of(context).state.uri.path,
+            );
+            await Clipboard.setData(ClipboardData(text: url));
+
+            if (!context.mounted) {
+              return;
+            }
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('URLをコピーしました'),
+                behavior: SnackBarBehavior.floating,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            );
+          },
+          leadingIcon: const Icon(Icons.link),
+          child: const Text('URLをコピーする'),
+        ),
+        MenuItemButton(onPressed: () {}, child: const Text('Xに投稿する')),
+      ],
+      builder: (_, MenuController controller, Widget? child) {
+        return IconButton(
+          focusNode: _buttonFocusNode,
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+          icon: const Icon(Icons.share),
+        );
+      },
     );
   }
 }
