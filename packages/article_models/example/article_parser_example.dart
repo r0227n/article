2e3e9ff8e920d6941d971a1d9572e4cf6bbb8e16
r0227n <developryo@gmail.com>
2025-04-13 12:40:53 +0900

    rename project

---
+++ packages/article_models/example/article_parser_example.dart
@@ -0,0 +1,6 @@
+import 'package:article_models/article_models.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/article_models/lib/src/models/article_metadata.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'article_metadata.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_ArticleMetadata _$ArticleMetadataFromJson(Map<String, dynamic> json) =>
+    _ArticleMetadata(
+      filePath: json['filePath'] as String,
+      year: (json['year'] as num).toInt(),
+      month: (json['month'] as num).toInt(),
+      day: (json['day'] as num).toInt(),
+      title: json['title'] as String,
+      emoji: json['emoji'] as String,
+      type: json['type'] as String,
+      topics:
+          (json['topics'] as List<dynamic>).map((e) => e as String).toList(),
+      published: json['published'] as bool? ?? true,
+    );
+
+Map<String, dynamic> _$ArticleMetadataToJson(_ArticleMetadata instance) =>
+    <String, dynamic>{
+      'filePath': instance.filePath,
+      'year': instance.year,
+      'month': instance.month,
+      'day': instance.day,
+      'title': instance.title,
+      'emoji': instance.emoji,
+      'type': instance.type,
+      'topics': instance.topics,
+    };
