8d2ff24daea33c282e66d7bef5ba257a2bdcc3f3
r0227n <developryo@gmail.com>
2025-04-13 20:54:30 +0900

    refactor: simplify baseWebPath retrieval and enhance file loading method for web

--- app/article_viewer/lib/data/services/asset_loader_helper.dart
+++ app/article_viewer/lib/data/services/asset_loader_helper.dart
@@ -3,29 +3,18 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 class AssetLoaderHelper {
-  /// GitHub Pagesのリポジトリ名を含むベースパスを取得
-  static String get baseWebPath {
-    if (!kIsWeb) return '';
-
-    // 例：https://username.github.io/repo-name/ の場合
-    final uri = Uri.base;
-    final pathSegments = uri.pathSegments.where((s) => s.isNotEmpty).toList();
-
-    if (pathSegments.isEmpty) return '/';
-    // リポジトリ名を最初のセグメントとして使用
-    return '/${pathSegments[0]}/';
-  }
+  const AssetLoaderHelper._();
 
   /// ファイルをロードする汎用メソッド
   static Future<String> loadFile(String relativePath) async {
-    // 全角文字が含まれていても正常にリクエストできるようにエンコード
-    final encodedPath = Uri.encodeFull(relativePath);
-    final response = await http.get(Uri.https(Uri.base.host, encodedPath));
-    if (response.statusCode == 200) {
-      return response.body;
-    }
     if (kIsWeb && !Uri.base.host.contains('localhost')) {
       // Webの場合、まずHTTPでロードを試みる
+
+      final encodedPath = Uri.encodeFull(relativePath);
+      final response = await http.get(Uri.https(Uri.base.host, encodedPath));
+      if (response.statusCode == 200) {
+        return response.body;
+      }
     }
 
     // Webでないか、HTTPロードに失敗した場合はassetからロード
